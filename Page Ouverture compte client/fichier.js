ea14d63867364eac4be4e7f51336b97ec27604c8
Aissata Ba <aissata.ba0903@gmail.com>
2020-06-18 09:37:26 +0000

    changement de formulaire

--- Page Ouverture compte client/fichier.js
+++ Page Ouverture compte client/fichier.js
@@ -8,17 +8,36 @@
 
 });*/
 
+ var radios1 = document.querySelectorAll('.typeClient');
+ for (i = 0; i < radios1.length; i++){
+     radios1[i].addEventListener('click',function(){
+         var id = this.getAttribute('id');
+         console.log(id);
+         var formTypeClient = document.querySelector('.'+ id);
+         var hiddens = document.querySelectorAll('.hidden');
+         for (i = 0; i < hiddens.length; i++) {
+            hiddens[i].style.display = 'none';
+        }
+        formTypeClient.style.display = 'block';
+        var hidden1 = document.querySelector('.hidden1');
+        hidden1.style.display = 'none';
+     })
+ }
+
+
 var radios = document.querySelectorAll('.typeCompte');
 for (i = 0; i < radios.length; i++) {
     radios[i].addEventListener('click', function () {
         var id = this.getAttribute('id');
         var divTypeCompte = document.querySelector('.' + id);
-        var hiddens = document.querySelectorAll('.hidden');
-        for (i = 0; i < hiddens.length; i++) {
-            hiddens[i].style.display = 'none';
-        }
+        var champsCompteCourant = document.querySelector('.compte_courant');
+        champsCompteCourant.style.display = 'none';
+        var champsCompteBloque = document.querySelector('.compte_bloque');
+        champsCompteBloque.style.display = 'none';
         divTypeCompte.style.display = 'block';
         var button_valider = document.querySelector('.button_valider')
         button_valider.style.display = 'block';
     });
 }
+
+
