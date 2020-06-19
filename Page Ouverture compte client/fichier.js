8eaca547bc1b60eb5ea250626ffe21c079f80e23
Aissata Ba <aissata.ba0903@gmail.com>
2020-06-19 12:30:33 +0000

    verification saisi

--- Page Ouverture compte client/fichier.js
+++ Page Ouverture compte client/fichier.js
@@ -1,16 +1,9 @@
-/*$(document).ready(function(){
 
-    $('.typeCompte').click(function() {
-        var id = $(this).attr('id');
-        $('.hidden').hide();
-        $('.' + id).show();
-    });
+    // Pour selectionner le type de client(particulier ou entreprise)
 
-});*/
-
- var radios1 = document.querySelectorAll('.typeClient');
- for (i = 0; i < radios1.length; i++){
-     radios1[i].addEventListener('click',function(){
+    var radios1 = document.querySelectorAll('.typeClient');
+        for (i = 0; i < radios1.length; i++){
+        radios1[i].addEventListener('click',function(){
          var id = this.getAttribute('id');
          console.log(id);
          var formTypeClient = document.querySelector('.'+ id);
@@ -23,6 +16,8 @@
         hidden1.style.display = 'none';
      })
  }
+
+ // Pour selectionner le type de compte(courant, bloqué, epargne)
 
 
 var radios = document.querySelectorAll('.typeCompte');
@@ -39,5 +34,135 @@ for (i = 0; i < radios.length; i++) {
         button_valider.style.display = 'block';
     });
 }
+
+
+// traitement choix ajouter client entreprise
+document.forms['form3'].addEventListener('submit', function(e)
+{
+    var erreur;
+    var inputs = document.querySelectorAll('.ClientEntreprise input');
+    console.log(inputs);
+    for (i=0;i < inputs.length; i++){
+        if (!inputs[i].value){
+            erreur = 'Veuillez renseigez tous les champs!';
+            break;
+
+        }
+    }
+    console.log(erreur);
+    if (erreur) {
+		document.getElementById("erreur").innerHTML = erreur;
+        return false;
+        
+    } else{
+       
+        document.getElementById("erreur").innerHTML = "";
+        alert('Compte creer!');
+
+        var inputs = document.querySelectorAll('.ClientEntreprise input');
+        console.log(inputs);
+        for (i=0;i < inputs.length; i++){
+            inputs[i].value = "";
+        }
+
+     }
+     
+});
+
+// traitement choix ajouter client particulier
+document.forms['form2'].addEventListener('submit', function(e)
+{
+    
+    var erreur;
+    var inputs = document.querySelectorAll('.clientParticulier input');
+    for (i=0;i < inputs.length; i++){
+        if (!inputs[i].value){
+            erreur = 'Veuillez renseigez tous les champs!';
+            break;
+
+        }
+    }
+    if (erreur) {
+		document.getElementById("erreur").innerHTML = erreur;
+		return false;
+    } else{
+       
+        document.getElementById("erreur").innerHTML = "";
+        alert('Compte creer!');
+
+        var inputs = document.querySelectorAll('.ClientParticulier input');
+        for (i=0;i < inputs.length; i++){
+            inputs[i].value = "";
+        }
+
+     }
+});
+
+// Les fonctions de verifications de champs
+
+function surligne(champ, erreur)
+{
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor = "";
+}
+
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifTEL(champ)
+{
+  var regex = /^(77|78|70|76)[0-9]{7}$/;
+    
+    if (!regex.test (champ.value))
+    {
+        surligne(champ, true);
+     return false;
+    }
+    else
+    {
+        return(false);
+    }
+}
+function verifCNI(champ)
+{
+   if(champ.value.length < 13 || champ.value.length > 13)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+ 
+
+
+
+// choix chekhbox avec jquery 
+/*$(document).ready(function(){
+
+    $('.typeCompte').click(function() {
+        var id = $(this).attr('id');
+        $('.hidden').hide();
+        $('.' + id).show();
+    });
+
+});*/
 
 
